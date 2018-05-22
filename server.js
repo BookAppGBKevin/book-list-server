376aadc6df03faaa4f551ef483b587416f6f2084
GoldbeardSea <timbusch1@gmail.com>
2018-05-22 12:06:52 -0700

    putting in update for server

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 'use strict';
 
+export PORT=3000
+Mac:     export DATABASE_URL=postgres://localhost:5432/books_app
+Windows: export DATABASE_URL=postgres://postgres:postgresGB@localhost:5432/books_app
+
 const express = require('express');
 const cors = require('cors');
 const pg = require('pg');
 
 const app = express();
 const PORT = process.env.PORT;
+
 
 const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
